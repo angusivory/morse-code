f65b4e9a0ddf11edbf29654362c6644bd585b874
Angus Ivory <angus@tusks.org.uk>
2020-11-19 19:22:40 +0000

    initial file

---
+++ MorseDecoder/MorseDecoder.ino
@@ -0,0 +1,120 @@
+/* Morse Decoder
+ * -... .- -- --- .. ... -- -.-- .-.. .. - - .-.. . -... .- -... -.-- -... --- -.--
+ * 
+ * three buttons - dot, dash and enter
+ * each time enter is pressed work out the character and display it
+ * 
+ * https://www.arduino.cc/en/Tutorial/LibraryExamples/HelloWorld
+*/
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int dotPin = 1;
+int dashPin = 2;
+int enterPin = 3;
+String character = "";
+String letters = "";
+String morse[] = {
+  ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----" 
+};
+char alphabet[] = {
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+};
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(dotPin, INPUT); 
+  pinMode(dashPin, INPUT);
+  pinMode(enterPin, INPUT);
+  //while(!Serial) {} //Wait for the user to open the serial monitor
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.noBlink();
+  lcd.print("Input morse code");
+  lcd.setCursor(0, 1);
+}
+
+void loop() {
+  receiveInput();
+};
+
+void receiveInput() {
+  //get each button press: if dot or dash, add it to character, if enter, character is complete so processChar()
+  if (digitalRead(dotPin) == LOW) {
+    character += ".";
+    //make a beep sound
+  } else if (digitalRead(dashPin) == LOW) {
+    character += "-";
+    //make a beeeeep sound
+  } else if (digitalRead(enterPin) == LOW) {
+    processChar();
+    //make some kind of beep sound
+  }
+};
+
+void processChar() {
+  //once the enter button is pressed:
+  //convert the morse to english
+  //display the morse
+  
+  for(int i = 0; i <= 36; i++) { //check if morse string is in the morse list
+    if (morse[i] == character) {
+      character = alphabet[i]; //set character to corresponding english letter
+      letters += character;
+      Serial.println(character);
+      dispChar(); //display the character
+    }
+  };
+  
+  character = ""; //reset variable for next character input
+};
+
+void dispChar() {
+  //https://www.arduino.cc/en/Tutorial/LibraryExamples/LiquidCrystalAutoscroll
+  
+  if (letters.length() >= 16) {
+    lcd.autoscroll();
+  };
+  lcd.print(character);
+};
+
+
+/* Basic Digital Read
+ * ------------------ 
+ *
+ * turns on and off a light emitting diode(LED) connected to digital  
+ * pin 13, when pressing a pushbutton attached to pin 7. It illustrates the
+ * concept of Active-Low, which consists in connecting buttons using a
+ * 1K to 10K pull-up resistor.
+ *
+ * Created 1 December 2005
+ * copyleft 2005 DojoDave <http://www.0j0.org>
+ * http://arduino.berlios.de
+ *
+ *
+
+int ledPin = 13; // choose the pin for the LED
+int inPin = 7;   // choose the input pin (for a pushbutton)
+int val = 0;     // variable for reading the pin status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);  // declare LED as output
+  pinMode(inPin, INPUT);    // declare pushbutton as input
+}
+
+void loop(){
+  val = digitalRead(inPin);  // read input value
+  if (val == HIGH) {         // check if the input is HIGH (button released)
+    digitalWrite(ledPin, LOW);  // turn LED OFF
+  } else {
+    digitalWrite(ledPin, HIGH);  // turn LED ON
+  }
+}
+*/
